b916844001c13cd7300a0aebb31b8fbb18b5f5a6
Oleksandr Yepik <sepika534@gmail.com>
2026-01-07 21:11:11 +0200

    install react-datepicker package

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -5,8 +5,8 @@ import Logo from "@/assets/image/Logo.svg";
 
 const Header = () => {
   return (
-    <div className="w-full h-43 shadow-[0_4px_24px_0_rgba(0,0,0,0.12),inset_0_12px_24px_0_rgba(0,0,0,0.12)] bg-[rgba(255,255,255,0.67)]">
-      <Grid>
+    <div className="w-full h-43 py-1.5 shadow-[0_4px_24px_0_rgba(0,0,0,0.12),inset_0_12px_24px_0_rgba(0,0,0,0.12)] bg-[rgba(255,255,255,0.67)]">
+      <Grid className="items-center">
         <div className="col-span-3 ">
           <Image
             src={Logo}
@@ -14,7 +14,7 @@ const Header = () => {
             className="ml-13.5"
           />
         </div>
-        <div className="col-span-6 col-start-5">
+        <div className="col-span-6 col-start-5 ">
           <div className="flex items-center ">
             <h3 className="font-sans font-medium text-2xl">monblanproject</h3>
             <div className="flex items-center justify-center border-[#3d8eda] h-7 px-3 border rounded-xs ml-4 ">
@@ -23,6 +23,22 @@ const Header = () => {
                 Start on 17-02-2016
               </p>
             </div>
+          </div>
+
+          <div className="flex items-center gap-5 mt-4">
+            <p className="font-serif font-normal text-[14px] ">
+              <span className="font-bold">870</span> posts
+            </p>
+            <p className="font-serif font-normal text-[14px] ">
+              <span className="font-bold">11,787</span> followers
+            </p>
+            <p className="font-serif font-normal text-[14px] ">
+              <span className="font-bold">112</span> following
+            </p>
+          </div>
+
+          <div className="flex items-center gap-5 mt-5">
+            <p className="font-serif text-[16px] font-normal">Date</p>
           </div>
         </div>
       </Grid>
